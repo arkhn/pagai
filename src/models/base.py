92136e3be1ecf657595e02f20d3aa887b9fd88b9
Theo <theo.leffyr@gmail.com>
2018-11-14 10:40:09 +0100

    Update classification to include proba scores. Sort results w. score

--- src/models/base.py
+++ src/models/base.py
@@ -1,12 +1,20 @@
 from src import loader
+import operator
 
 
 class BaseClassifier:
     def __init__(self):
         self.classification = None
 
-    def find_all(self, resource_type):
+    def find_all(self, resource_type, limit=20):
         if self.classification is None:
-            raise TypeError('Model si not ready for live classification.')
-        columns = self.classification[resource_type]
-        return columns
+            raise TypeError('Model is not ready for live classification.')
+        results = []
+        for column_name, column in self.classification.items():
+            proba = column['labels'][resource_type]
+            if proba > 0.1:
+                results.append([proba, column_name, column])
+
+        results.sort(key=operator.itemgetter(0), reverse=True)
+
+        return results[:limit]

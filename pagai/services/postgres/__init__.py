e63dd4d40528be27afa0eda8f80fb41a9841ac55
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-07-27 11:35:27 +0200

    Lighter Pagai (#43)
    * Remove dependencies to build a light docker image
    
    * missing pandas dep
    
    * try
    
    * Remove dependencies to build a light docker image
    
    * missing pandas dep
    
    * try
    
    * fix a conflict
    
    * remove prints
    
    * integrate casing changes to db_Schema
    
    Co-authored-by: simonvadee <simon.vadee@gmail.com>

--- pagai/services/postgres/__init__.py
+++
@@ -1,9 +0,0 @@
-from pagai.services.postgres.main import (
-    explore,
-    fetch_columns,
-    from_pyrog_credentials,
-    get_table_names,
-    get_column_names,
-    get_column,
-    get_table,
-)
